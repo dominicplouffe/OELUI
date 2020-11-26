47745567920eb9bf59e91bea848b34fbcee911d1
Dominic Plouffe <dominic@dplouffe.ca>
2020-11-25 19:30:49 -0500

    removed pro fontawesome as they are not used

--- src/App.js
+++ src/App.js
@@ -20,9 +20,6 @@ import NewPing from "./dashboard/pages/NewPing";
 import Profile from "./dashboard/pages/Profile";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/pro-solid-svg-icons";
-import { fad } from "@fortawesome/pro-duotone-svg-icons";
-import { far } from "@fortawesome/pro-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 
 library.add(fas);

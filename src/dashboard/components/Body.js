f22de6b83ba1e09224de19632c225085e7afcf16
Dominic Plouffe <dominic@dplouffe.ca>
2020-12-28 15:56:35 -0500

    improved look and feel a bit

--- src/dashboard/components/Body.js
+++ src/dashboard/components/Body.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Button, Alert } from "react-bootstrap";
+import { Row, Col, Navbar, Alert } from "react-bootstrap";
 
 const Body = ({
   currentUser,
@@ -53,284 +53,223 @@ const Body = ({
 
   return (
     <>
-      <div
-        id="main-wrapper"
-        data-theme="dark"
-        data-layout="vertical"
-        data-navbarbg="skin6"
-        data-sidebartype={sideBarType}
-        data-sidebar-position="fixed"
-        data-header-position="fixed"
-        data-boxed-layout="full"
-      >
-        <header className="topbar" data-navbarbg="skin6">
-          <nav className="navbar top-navbar navbar-expand-md">
-            <div className="navbar-header" data-logobg="skin6">
-              {/* This is for the sidebar toggle which is visible on mobile only */}
-              <Button
-                variant="link"
-                className="nav-toggler waves-effect waves-light d-block d-md-none"
-                onClick={() => hamburgerClick()}
-              >
-                <i className="ti-menu ti-close" style={{ color: "#FFF" }}></i>
-              </Button>
-              <div className="navbar-brand text-center">
-                <img
-                  src="https://dplouffe.ca/static/img/domlogo.png"
-                  alt="wrapkit"
-                  style={{ maxWidth: "100px", margin: "auto" }}
-                />
-              </div>
-            </div>
-
-            <div
-              className="navbar-collapse collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav float-left mr-auto ml-3 pl-1">
-                <li className="nav-item d-none d-md-block">
-                  <h3 className="page-title text-truncate text-light font-weight-medium mb-1">
-                    {currentUser.role.org.name}
-                  </h3>
-                </li>
-              </ul>
-
-              <ul className="navbar-nav float-right">
-                <li className="nav-item dropdown">
-                  <span className="ml-2 d-none d-lg-inline-block">&nbsp;</span>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-        <aside
-          className="left-sidebar"
-          data-sidebarbg="skin6"
-          style={{ position: "fixed", left: sideBarLeft }}
-        >
-          <div
-            className="scroll-sidebar ps-container ps-theme-default ps-active-y"
-            data-sidebarbg="skin6"
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+          <img
+            src="https://dplouffe.ca/static/img/domlogo.png"
+            alt="wrapkit"
+            style={{ maxWidth: "100px", margin: "auto", float: "left" }}
+          />
+          <span
+            className="pl-3 h3 mt-3 hide-small"
+            style={{ color: "#000", float: "left" }}
           >
-            <nav
-              className="sidebar-nav"
-              style={{ overflowY: "auto", height: "100%" }}
-            >
-              <ul id="sidebarnav">
-                <li className={`sidebar-item ${getSelectedMenuClass("ping")}`}>
-                  <Link
-                    className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                      "ping"
-                    )}`}
-                    to="/pings"
-                  >
-                    <img
-                      src="https://onerrorlog.s3.amazonaws.com/images/ping.png"
-                      alt="pings"
-                      className="icon"
-                    />
-                    <span className="hide-menu">Pings</span>
-                  </Link>
-                </li>
-                <li className={`sidebar-item ${getSelectedMenuClass("pong")}`}>
-                  <Link
-                    className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                      "pong"
-                    )}`}
-                    to="/pongs"
-                  >
-                    <img
-                      src="https://onerrorlog.s3.amazonaws.com/images/pong.png"
-                      alt="pings"
-                      className="icon"
-                    />
-                    <span className="hide-menu">Pongs</span>
-                  </Link>
-                </li>
+            onErrorLog
+          </span>
+        </a>
+        {/* <button
+          className="navbar-toggler position-absolute d-md-none collapsed"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#sidebarMenu"
+          aria-controls="sidebarMenu"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button> */}
 
+        {/* <Navbar.Toggle aria-controls="sidebarMenu" /> */}
+
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <Link className="nav-link" to="/auth/logout">
+              <div className="top-link">
+                <img
+                  src="https://onerrorlog.s3.amazonaws.com/images/logout.png"
+                  alt="logout"
+                  className="icon"
+                />
+                &nbsp; Logout
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </header>
+
+      <div className="container-fluid">
+        <Row>
+          <nav
+            id="sidebarMenu"
+            className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse mt-3 sidebar-nav"
+          >
+            <ul id="sidebarnav">
+              <li className={`sidebar-item ${getSelectedMenuClass("ping")}`}>
+                <Link
+                  className={`sidebar-link sidebar-link ${getActiveMenuClass(
+                    "ping"
+                  )}`}
+                  to="/pings"
+                >
+                  <img
+                    src="https://onerrorlog.s3.amazonaws.com/images/ping.png"
+                    alt="pings"
+                    className="icon"
+                  />
+                  <span className="hide-menu">Pings</span>
+                </Link>
+              </li>
+              <li className={`sidebar-item ${getSelectedMenuClass("pong")}`}>
+                <Link
+                  className={`sidebar-link sidebar-link ${getActiveMenuClass(
+                    "pong"
+                  )}`}
+                  to="/pongs"
+                >
+                  <img
+                    src="https://onerrorlog.s3.amazonaws.com/images/pong.png"
+                    alt="pings"
+                    className="icon"
+                  />
+                  <span className="hide-menu">Pongs</span>
+                </Link>
+              </li>
+
+              <li className={`sidebar-item ${getSelectedMenuClass("vitals")}`}>
+                <Link
+                  className={`sidebar-link sidebar-link ${getActiveMenuClass(
+                    "vitals"
+                  )}`}
+                  to="/vitals"
+                >
+                  <img
+                    src="https://onerrorlog.s3.amazonaws.com/images/vitals.png"
+                    alt="vitals"
+                    className="icon"
+                  />
+                  <span className="hide-menu">Vitals</span>
+                </Link>
+              </li>
+
+              <li className="list-divider"></li>
+              {currentUser.role.role === "admin" && (
+                <li className={`sidebar-item ${getSelectedMenuClass("team")}`}>
+                  <Link
+                    className={`sidebar-link sidebar-link ${getActiveMenuClass(
+                      "team"
+                    )}`}
+                    to="/team"
+                  >
+                    <img
+                      src="https://onerrorlog.s3.amazonaws.com/images/teams.png"
+                      alt="team"
+                      className="icon"
+                    />
+                    <span className="hide-menu">Team</span>
+                  </Link>
+                </li>
+              )}
+
+              {currentUser.role.role === "admin" && (
                 <li
-                  className={`sidebar-item ${getSelectedMenuClass("vitals")}`}
+                  className={`sidebar-item ${getSelectedMenuClass("schedule")}`}
                 >
                   <Link
                     className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                      "vitals"
-                    )}`}
-                    to="/vitals"
-                  >
-                    <img
-                      src="https://onerrorlog.s3.amazonaws.com/images/vitals.png"
-                      alt="vitals"
-                      className="icon"
-                    />
-                    <span className="hide-menu">Vitals</span>
-                  </Link>
-                </li>
-
-                <li className="list-divider"></li>
-                {currentUser.role.role === "admin" && (
-                  <li
-                    className={`sidebar-item ${getSelectedMenuClass("team")}`}
-                  >
-                    <Link
-                      className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                        "team"
-                      )}`}
-                      to="/team"
-                    >
-                      <img
-                        src="https://onerrorlog.s3.amazonaws.com/images/teams.png"
-                        alt="team"
-                        className="icon"
-                      />
-                      <span className="hide-menu">Team</span>
-                    </Link>
-                  </li>
-                )}
-
-                {currentUser.role.role === "admin" && (
-                  <li
-                    className={`sidebar-item ${getSelectedMenuClass(
                       "schedule"
                     )}`}
-                  >
-                    <Link
-                      className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                        "schedule"
-                      )}`}
-                      to="/schedule"
-                    >
-                      <img
-                        src="https://onerrorlog.s3.amazonaws.com/images/schedule.png"
-                        alt="schedule"
-                        className="icon"
-                      />
-                      <span className="hide-menu">Schedule</span>
-                    </Link>
-                  </li>
-                )}
-
-                {/* {currentUser.role.role === "admin" && (
-                  <li
-                    className={`sidebar-item ${getSelectedMenuClass(
-                      "preferences"
-                    )}`}
-                  >
-                    <Link
-                      className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                        "preferences"
-                      )}`}
-                      to="/preferences"
-                    >
-                      <img
-                        src="https://onerrorlog.s3.amazonaws.com/images/preferences.png"
-                        alt="preferences"
-                        className="icon"
-                      />
-                      <span className="hide-menu">Preferences</span>
-                    </Link>
-                  </li>
-                )} */}
-
-                <li
-                  className={`sidebar-item ${getSelectedMenuClass("profile")}`}
-                >
-                  <Link
-                    className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                      "profile"
-                    )}`}
-                    to="/profile"
+                    to="/schedule"
                   >
                     <img
-                      src="https://onerrorlog.s3.amazonaws.com/images/profile.png"
-                      alt="profile"
+                      src="https://onerrorlog.s3.amazonaws.com/images/schedule.png"
+                      alt="schedule"
                       className="icon"
                     />
-                    <span className="hide-menu">Profile</span>
-                    {currentUser && currentUser.role.phone_number === null && (
-                      <span
-                        role="img"
-                        aria-label="Mising Phone Number"
-                        className="ml-1 hide-menu"
-                      >
-                        ☎️
-                      </span>
-                    )}
+                    <span className="hide-menu">Schedule</span>
                   </Link>
                 </li>
-                <li className="list-divider"></li>
-                <li
-                  className={`sidebar-item ${getSelectedMenuClass("signout")}`}
+              )}
+
+              <li className={`sidebar-item ${getSelectedMenuClass("profile")}`}>
+                <Link
+                  className={`sidebar-link sidebar-link ${getActiveMenuClass(
+                    "profile"
+                  )}`}
+                  to="/profile"
                 >
-                  <Link
-                    className={`sidebar-link sidebar-link ${getActiveMenuClass(
-                      "signout"
-                    )}`}
-                    to="/auth/logout"
-                  >
-                    <img
-                      src="https://onerrorlog.s3.amazonaws.com/images/logout.png"
-                      alt="logout"
-                      className="icon"
-                    />
-                    <span className="hide-menu">Logout</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </aside>
-        <div className="page-wrapper" style={{ display: "block" }}>
-          <div className="container-fluid">
-            <Row>
-              <Col xl={{ span: 10 }}>
-                {currentUser &&
-                  currentUser.role.phone_number_verified_on === null && (
-                    <Alert variant="info" dismissible>
-                      <Alert.Heading>
-                        <strong>Confirm Your Phone Number</strong>
-                      </Alert.Heading>
-                      <p>
-                        It looks like you have not yet confirmed your phone
-                        number. If you want to receive alerts with a text
-                        message, please confirm your phone number in your{" "}
-                        <Link to="/profile">
-                          <strong>profile</strong>
-                        </Link>
-                      </p>
-                    </Alert>
+                  <img
+                    src="https://onerrorlog.s3.amazonaws.com/images/profile.png"
+                    alt="profile"
+                    className="icon"
+                  />
+                  <span className="hide-menu">Profile</span>
+                  {currentUser && currentUser.role.phone_number === null && (
+                    <span
+                      role="img"
+                      aria-label="Mising Phone Number"
+                      className="ml-1 hide-menu"
+                    >
+                      ☎️
+                    </span>
                   )}
-                {loading ? `loading....` : children}
-              </Col>
-            </Row>
-          </div>
-          <div className="footer text-center">
-            &copy; Copyright{" "}
-            <a
-              href="https://www.dplouffe.ca"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              dplouffe Analytics
-            </a>
-            . All rights reserved.
-            <br />
-            <a
-              href="https://www.dplouffe.ca"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Privacy Policy
-            </a>{" "}
-            |{" "}
-            <a
-              href="https://www.dplouffe.ca"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Terms of Use
-            </a>
-          </div>
-        </div>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+              <Row className="w-100" noGutters>
+                <Col>
+                  {currentUser &&
+                    currentUser.role.phone_number_verified_on === null && (
+                      <Alert variant="info" dismissible>
+                        <Alert.Heading>
+                          <strong>Confirm Your Phone Number</strong>
+                        </Alert.Heading>
+                        <p>
+                          It looks like you have not yet confirmed your phone
+                          number. If you want to receive alerts with a text
+                          message, please confirm your phone number in your{" "}
+                          <Link to="/profile">
+                            <strong>profile</strong>
+                          </Link>
+                        </p>
+                      </Alert>
+                    )}
+                  {loading ? `loading....` : children}
+                </Col>
+              </Row>
+            </div>
+            <div className="footer text-center">
+              &copy; Copyright{" "}
+              <a
+                href="https://www.dplouffe.ca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                dplouffe Analytics
+              </a>
+              . All rights reserved.
+              <br />
+              <a
+                href="https://www.dplouffe.ca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Privacy Policy
+              </a>{" "}
+              |{" "}
+              <a
+                href="https://www.dplouffe.ca"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Terms of Use
+              </a>
+            </div>
+          </main>
+        </Row>
       </div>
     </>
   );
